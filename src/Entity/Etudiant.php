2c4733d656475cfc428d82be40f96161ae290f29
Ophelie <freville.ophelie@neuf.fr>
2024-01-25 10:10:13 +0100

    Ajout du formulaire d'inscription

--- src/Entity/Etudiant.php
+++ src/Entity/Etudiant.php
@@ -4,10 +4,12 @@ namespace App\Entity;
 
 use App\Repository\EtudiantRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: EtudiantRepository::class)]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Etudiant implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
