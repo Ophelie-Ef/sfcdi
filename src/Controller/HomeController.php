03d528d5d1aa5685f3e32126fc84402c43025fc8
Ophelie <freville.ophelie@neuf.fr>
2024-01-26 10:01:47 +0100

    Ajout lien Home sur le logo, et son css  ( invisible pour gitHub ^^ )

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        return $this->render('home/index.html.twig');
     }
+
+    // #[Route('/secret', name: 'app_home_access')]
+    // #[IsGranted('')]
 }
