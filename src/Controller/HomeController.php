b7dc3f0512b8152cb247b7ecd3c7a2ea58e112b5
Ophelie <freville.ophelie@neuf.fr>
2024-02-07 14:14:43 +0100

    Pagination page d'accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,21 +3,55 @@
 namespace App\Controller;
 
 use App\Repository\LivreRepository;
+use Knp\Component\Pager\Paginator;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class HomeController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function index(LivreRepository $livreRepository): Response
-    {
-        return $this->render('home/index.html.twig',[
-            'livres' => $livreRepository->findAll()
+    #[Route('/', name: 'app_home', methods: ['GET'])]
+    public function index(
+        LivreRepository $livreRepository,
+        PaginatorInterface $paginator,
+        Request $request
+    ): Response {
+
+        $livres = $paginator->paginate(
+            $livreRepository->findBy([], ['id' => 'DESC']),
+            $request->query->getInt('page', 1),
+            10
+        );
+
+        return $this->render('home/index.html.twig', [
+            'livres' => $livreRepository->findAll(),
+            'livres' => $livres,
         ]);
     }
 
     // #[Route('/secret', name: 'app_home_access')]
     // #[IsGranted('')]
 }
+
+
+// #[Route('/', name: 'app_livre_index', methods: ['GET'])]
+// public function index(
+//     LivreRepository $livreRepository,
+//     PaginatorInterface $paginator,
+//     Request $request
+// ): Response {
+
+//     $livres = $paginator->paginate(
+//         $livreRepository->findBy([], ['id' => 'DESC']),
+//         $request->query->getInt('page', 1),
+//         10
+//     );
+
+//     return $this->render('livre/index.html.twig', [
+//         'livres' => $livreRepository->findAll(),
+//         'livres' => $livres,
+//     ]);
+// }
